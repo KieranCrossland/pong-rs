78da54254be68eefb62006c163763df3e7045101
KieranCrossland <kierancrossland1st@gmail.com>
2023-09-12 20:31:43 +0100

    rename players with configfiles

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use macroquad::prelude::*;
+use std::fs;
 use std::process;
 mod ball;
 mod paddle;
 
 #[macroquad::main("Kieran's Pong in Rust (macroquad)")]
 async fn main() {
+    
     let mut player1 = paddle::Paddle {
-        name: "player1".to_string(),
+        name: fs::read_to_string("config/player/player1.name").expect("Unable to read file: config/player/player1.name\n"),
         xpos: 10.0,
         ypos: screen_height() / 2.0 - 70.0,
         width: 20.0,
@@ -16,7 +18,7 @@ async fn main() {
     };
 
     let mut player2 = paddle::Paddle {
-        name: "player2".to_string(),
+        name: fs::read_to_string("config/player/player2.name").expect("Unable to read file: config/player/player2.name\n"),
         xpos: screen_width() - 30.0,
         ypos: screen_height() / 2.0 - 70.0,
         width: 20.0,
@@ -33,12 +35,13 @@ async fn main() {
         speed: 2.0,
         colour: WHITE,
     };
-
-    println!("(Game) Entering gameloop");
+    
     player1.log_position();
     player2.log_position();
     ball1.log_position();
-    loop {
+    
+    println!("(Game) Entering gameloop");
+    loop { 
         clear_background(BLACK);
         //system keybinds
         if is_key_down(KeyCode::Escape) {
